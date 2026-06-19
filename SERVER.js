1f2324df837db2338e89e2efdac154ec3d2cf449
Matheraptor <hamedahastral@gmail.com>
2026-06-19 05:35:03 +0200

    temp removal of SERVER.TEST for remote server

--- SERVER.js
+++ SERVER.js
@@ -585,21 +585,21 @@ MAGPIE_SERVER.SOCKET.auth_dev = function auth_dev(socketID) {
 // #region > Tests
 //------------------------------------------------------------------------
 MAGPIE_SERVER.TEST = {};
-MAGPIE_SERVER.TEST.meta = {
-  name: MAGPIE.meta.name + "testing",
-};
-MAGPIE_SERVER.TEST.Path = "\\.tests";
-MAGPIE_SERVER.TEST.list = fs
-  .readdirSync(path.join(process.cwd(), MAGPIE_SERVER.TEST.Path))
-  .filter((string) => string.includes(".js"));
-if (MAGPIE_SERVER.TEST.list && MAGPIE_SERVER.TEST.list.length > 0) {
-  MAGPIE_SERVER.TEST.list.forEach((testFile) => {
-    const testName = testFile.replace(".js", "");
-    console.log(`[TEST] processing ${testName}...`);
-    const directory = path.join(MAGPIE_SERVER.TEST.Path, testFile);
-    MAGPIE_SERVER.TEST[testName] = require(`.${directory}`);
-  });
-}
+// MAGPIE_SERVER.TEST.meta = {
+//   name: MAGPIE.meta.name + "testing",
+// };
+// MAGPIE_SERVER.TEST.Path = "\\.tests";
+// MAGPIE_SERVER.TEST.list = fs
+//   .readdirSync(path.join(process.cwd(), MAGPIE_SERVER.TEST.Path))
+//   .filter((string) => string.includes(".js"));
+// if (MAGPIE_SERVER.TEST.list && MAGPIE_SERVER.TEST.list.length > 0) {
+//   MAGPIE_SERVER.TEST.list.forEach((testFile) => {
+//     const testName = testFile.replace(".js", "");
+//     console.log(`[TEST] processing ${testName}...`);
+//     const directory = path.join(MAGPIE_SERVER.TEST.Path, testFile);
+//     MAGPIE_SERVER.TEST[testName] = require(`.${directory}`);
+//   });
+// }
 MAGPIE_SERVER.testLog = function (message, prefix = "log", error = "") {
   const callback = console[prefix];
   callback(message, error);
